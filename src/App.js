c4978f1d687e0d8fd2fa168b2c0353b233fa38c9
hahak94 <90976221+hahak94@users.noreply.github.com>
2022-10-31 12:50:40 +0200

    React-Tabs

--- src/App.js
+++ src/App.js
@@ -1,148 +1,158 @@
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
-import { AgGridReact } from'ag-grid-react'
-import'ag-grid-community/dist/styles/ag-grid.css'
-import'ag-grid-community/dist/styles/ag-theme-material.css';
+import { AgGridReact } from 'ag-grid-react'
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 // date-fns
 
 import DatePicker from "react-datepicker"
+import format from 'date-fns/format';
 import 'react-datepicker/dist/react-datepicker.css'
-import Stack from'@mui/material/Stack';
+import Stack from '@mui/material/Stack';
 
 
 import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
 import Button from '@mui/material/Button';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
-import TextField from'@mui/material/TextField';
-import Tabs from'@mui/material/Tabs';
-import Tab from'@mui/material/Tab';
- 
+import TextField from '@mui/material/TextField';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
 
 
 
 function App() {
-const [value, setPage] = useState('one')
-const handle =(event, value) => {
-  setPage(value)
-}
+  const [value, setPage] = useState('one')
+  const handle = (event, value) => {
+    setPage(value)
+  }
 
   const [uInput, setUInput] = useState({
     date: new Date(),
-    description:'',
-    priority:''
-    })
+    description: '',
+    priority: ''
+  })
 
   const setDate = (props) => {
-    setUInput({...uInput,  date : props })
-    
+    setUInput({ ...uInput, date: props })
+
   }
 
-  const [todos, setTodos] = useState([]) 
+  const [todos, setTodos] = useState([])
 
   const inputChange = (event) => {
-      setUInput({...uInput, [event.target.name]:event.target.value})
+    setUInput({ ...uInput, [event.target.name]: event.target.value })
   }
 
-  const handleClick = (event) => {  
-    event.preventDefault()  
-    setTodos([...todos,uInput]) 
+  const handleClick = (event) => {
+    event.preventDefault()
+    setTodos([...todos, uInput])
   }
 
   const columns = [
-    {headerName:'Date',field:'date', sortable: 'true', filter:'true', floatingFilter: true,animateRows:true},
-    {headerName:'Description',field:'description', sortable: 'true', filter:'true', floatingFilter: true,animateRows:true},
-    {headerName:'Priority',field:'priority', sortable: 'true', filter:'true', floatingFilter: true,animateRows:true,
-  cellStyle : params => params.value==="High" ? {color: 'red'} : {color:'blue'}}
-  ]  
+    { headerName: 'Date', field: 'date', sortable: 'true', filter: 'true', floatingFilter: true, animateRows: true,
+     valueFormatter: params => format(new Date(params.value), "dd.MM.yyyy") },
+    { headerName: 'Description', field: 'description', sortable: 'true', filter: 'true', floatingFilter: true, animateRows: true },
+    {
+      headerName: 'Priority', field: 'priority', sortable: 'true', filter: 'true', floatingFilter: true, animateRows: true,
+      cellStyle: params => params.value === "High" ? { color: 'red' } : { color: 'blue' }
+    }
+  ]
 
   const gridRef = useRef()
 
   const deleteTodo = () => {
     if (gridRef.current.getSelectedNodes().length > 0) {
-          setTodos(todos.filter((todo, index) => 
-               index !== gridRef.current.getSelectedNodes()[0].childIndex)) 
-               }
-    else {  
-        alert('Select row first');  
-      }
+      setTodos(todos.filter((todo, index) =>
+        index !== gridRef.current.getSelectedNodes()[0].childIndex))
     }
+    else {
+      alert('Select row first');
+    }
+  }
 
   return (
     <div className="App">
-      
-      <header className="App-header">   
-        <p> Simple To Do List App        
-        </p>    
-      </header>    
-      
-<Tabs value={value} onChange={handle}>
-  <Tab value="one" label="Home page"/>
-  <Tab value="two" label="Todo"/>
-</Tabs>
 
-{value === "one" &&<div> Welcome to Homepage </div>}
-{value === "two" &&
-<div>  
-        <p>Add to do</p>  
-<DatePicker
-           label ="Date"  
-           inputFormat="DD/MM/YYYY"
-           selected= {uInput.date}
-           renderInput={(props) => <TextField {...props} />}
-           onChange={ date =>{
-             setDate(date) 
+      <header className="App-header">
+        <p> Simple To Do List App
+        </p>
+      </header>
+
+      <Tabs value={value} onChange={handle}>
+        <Tab value="one" label="Home page" />
+        <Tab value="two" label="Todo" />
+      </Tabs>
+
+      {value === "one" && <div> Welcome to Homepage </div>}
+      {value === "two" &&
+        <div>
+          <p>Add to do</p>
+
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+
+          <DesktopDatePicker
+            label="Date"
+            inputFormat="DD/MM/YYYY"
+            value={uInput.date}
+            renderInput={(props) => <TextField {...props} />}
+            onChange={date => {
+              setDate(date)
             }
-           }
-           /> 
-           
-            <Stack direction="row" spacing={1} justifyContent="center" alignItems="center"> 
-            
-           
-           <TextField
-           label="Description"
-           variant="standard"
-           name="description"
-           value={uInput.description}
-           onChange={inputChange} />
-           
-           <TextField
-           label="Priority"
-           variant="standard"
-           name="priority"
-           value={uInput.priority}
-           onChange={inputChange} />
-       
-          
-       
-        <Button variant="outlined"  onClick={handleClick}>Add</Button>
-       
-   
-        <Button   variant="outlined" color="secondary" 
-        onClick={deleteTodo} endIcon={<DeleteTwoToneIcon/>} >Delete  </Button>
-       
+            }
+          />
+          </LocalizationProvider>
 
-        </Stack>
-       
+          <Stack direction="row" spacing={1} justifyContent="center" alignItems="center">
 
-     <div className="ag-theme-material" style={{height: '700px', width: '70%', margin: 'auto'}}> 
+            <TextField
+              label="Description"
+              variant="standard"
+              name="description"
+              value={uInput.description}
+              onChange={inputChange} />
 
-     <AgGridReact  
-      ref={gridRef} 
-      onGridReady={ params => gridRef.current = params.api } 
-      rowSelection="single" 
-      columnDefs={columns}  
-      rowData={todos}>
+            <TextField
+              label="Priority"
+              variant="standard"
+              name="priority"
+              value={uInput.priority}
+              onChange={inputChange} />
 
-        </AgGridReact>
- 
-      </div >
-      </div>}
-      
+
+
+            <Button variant="outlined" onClick={handleClick}>Add</Button>
+
+
+            <Button variant="outlined" color="secondary"
+              onClick={deleteTodo} endIcon={<DeleteTwoToneIcon />} >Delete  </Button>
+
+
+          </Stack>
+
+
+          <div className="ag-theme-material" style={{ height: '700px', width: '70%', margin: 'auto' }}>
+
+            <AgGridReact
+              ref={gridRef}
+              onGridReady={params => gridRef.current = params.api}
+              rowSelection="single"
+              columnDefs={columns}
+              rowData={todos}>
+
+            </AgGridReact>
+
+          </div >
+        </div>}
+
     </div>
-    
+
   )
-  
+
 }
 
-  
+
 export default App;
